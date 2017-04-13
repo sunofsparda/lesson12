3c5ed27b6e77ab0a40e808d8811b394f40af1fa9
Anton Cherlyonok <sunofsparda@gmail.com>
2017-04-13 23:10:59 +0300

    minor fixes 2

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,6 +1,6 @@
 node 'puppet-node1' {
   class { '::mysql::server':
-    root_password => 'root123',
+    root_password => 'password',
   }
 
 
@@ -10,18 +10,18 @@ node 'puppet-node1' {
   }
 
 
-  mysql_user { 'admin@localhost':
+  mysql_user { 'test_user@localhost':
     ensure  =>  present,
-    password_hash =>  mysql_password('qwe123'),
+    password_hash =>  mysql_password('test_user'),
   }
 
 
-  mysql_grant { 'admin@localhost/test_mdb.*':
+  mysql_grant { 'test_user@localhost/test_mdb.*':
     ensure  =>  present,
     options =>  ['GRANT'],
     privileges  =>  ['ALL'],
     table =>  'test_mdb.*',
-    user  =>  'admin@localhost',
+    user  =>  'test_user@localhost',
   }
 
 }
